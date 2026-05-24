e1425058d052e5aa318e75ec20646328cf64c20a
Sanjar <71312876+sanjarmedia@users.noreply.github.com>
2026-05-24 11:40:16 +0500

    Delete 01 directory

--- 01/client/src/utils/chunkArray.ts
+++
@@ -1,7 +0,0 @@
-export const chunkArray = (array: string[], size: number): string[][] => {
-   const result: string[][] = []
-   for (let i = 0; i < array.length; i += size) {
-      result.push(array.slice(i, i + size))
-   }
-   return result
-}
